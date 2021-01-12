ab09abc3bd29728eafeedf6d70fa00ba3e173a3d
Gastón Cerioni <dongatasoft@gmail.com>
2021-01-12 18:12:30 -0300

    Adds a sample event store

---
+++ src/goiar.simple.cqrs/Persistance/LoggerEventStore.cs
@@ -0,0 +1,31 @@
+﻿using Goiar.Simple.Cqrs.Entities;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Goiar.Simple.Cqrs.Persistance
+{
+    /// <summary>
+    /// Event Store implementation using <see cref="ILogger"/>
+    /// </summary>
+    public class LoggerEventStore : IEventStore
+    {
+        private readonly ILogger _logger;
+
+        public LoggerEventStore(ILogger<LoggerEventStore> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public Task Save(Event @event)
+        {
+            _logger.LogInformation($"Recieved an event with command name : {@event.CommandName}, {Environment.NewLine}" +
+                $"With content: {JsonConvert.SerializeObject(@event.Content, Formatting.Indented)} {Environment.NewLine}" +
+                $"With result: {JsonConvert.SerializeObject(@event.Result, Formatting.Indented)}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
