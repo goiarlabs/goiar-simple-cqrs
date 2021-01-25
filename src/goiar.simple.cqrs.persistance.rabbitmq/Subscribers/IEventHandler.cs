54fa86e9f809da1006ca221460b54ae6a8634aef
Gastón Cerioni <dongatasoft@gmail.com>
2021-01-25 12:50:49 -0300

    adds a missing summary

--- src/goiar.simple.cqrs.persistance.rabbitmq/Subscribers/IEventHandler.cs
+++ src/goiar.simple.cqrs.persistance.rabbitmq/Subscribers/IEventHandler.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace Goiar.Simple.Cqrs.persistance.rabbitmq.Subscribers
 {
+    /// <summary>
+    /// Interface that should implement a persistance method to save the recieved event
+    /// </summary>
     public interface IEventHandler
     {
         /// <summary>
