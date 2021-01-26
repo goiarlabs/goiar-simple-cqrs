13e7fd36209a20001fe0fb077ca7a25e54d0ac17
Gastón Cerioni <dongatasoft@gmail.com>
2021-01-26 17:32:52 -0300

    adds licenses

--- src/goiar.simple.cqrs.persistance.rabbitmq/RabbitMqEventStore.cs
+++ src/goiar.simple.cqrs.persistance.rabbitmq/RabbitMqEventStore.cs
@@ -2,15 +2,17 @@
 using Goiar.Simple.Cqrs.Entities;
 using Goiar.Simple.Cqrs.persistance.rabbitmq.Configurations;
 using Goiar.Simple.Cqrs.Persistance;
-using System;
 using System.Threading.Tasks;
 
 namespace Goiar.Simple.Cqrs.persistance.rabbitmq
 {
+    /// <summary>
+    /// <see cref="IEventStore"/> Imlementation for rabbitmq
+    /// </summary>
     public class RabbitMqEventStore : IEventStore
     {
         #region Fields
-        
+
         private readonly IBus _bus;
         private readonly QueueConfig _queueConfig;
 
@@ -18,6 +20,11 @@ namespace Goiar.Simple.Cqrs.persistance.rabbitmq
 
         #region Ctor
 
+        /// <summary>
+        /// Creates a new <see cref="RabbitMqEventStore"/>
+        /// </summary>
+        /// <param name="queueConfig"></param>
+        /// <param name="bus"></param>
         public RabbitMqEventStore(QueueConfig queueConfig, IBus bus = null)
         {
             _bus = bus;
@@ -34,6 +41,7 @@ namespace Goiar.Simple.Cqrs.persistance.rabbitmq
 
         #region Public methods
 
+        ///<inheritdoc/>
         public async Task Save(Event @event)
         {
 
