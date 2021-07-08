012dff46215dee5560ada22d160702cd25edfae8
otallante <otallante@goiar.com>
2021-07-08 17:11:44 -0300

    IEventQueue was added.

---
+++ src/goiar.simple.cqrs/Entities/IEventQueue.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Goiar.Simple.Cqrs.Entities
+{
+    /// <summary>
+    /// Contract to which any event queue should adhere.
+    /// </summary>
+    public interface IEventQueue
+    {
+        /// <summary>
+        /// Enqueues a new element onto the queue and releases the signal
+        /// </summary>
+        /// <param name="event"></param>
+        void Enqueue(Event @event);
+
+        /// <summary>
+        /// Waits until something is enqueued and enqueues it
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<Event> Dequeue(CancellationToken cancellationToken);
+    }
+}
